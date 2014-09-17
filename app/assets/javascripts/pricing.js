eacf31d82eec95ffe58c26fc3c1e2a88ac9f842a
JAdriamooneyC Group <adriamooney@Macintosh.local>
2014-09-16 17:42:04 -0700

    advanced mobile coupon

--- app/assets/javascripts/pricing.js
+++ app/assets/javascripts/pricing.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
 
   // Addons
   $('body').on('change', '.addons-list .checkbox', function() {
+    $('.addons-summary-container').empty();
     pricing.on('set.addon', addonHandler);
   });
 
@@ -22,7 +23,7 @@ $(document).ready(function() {
     console.log(addon);
     showAddonsSummary(addon);
     if (addon.quantity != 0) {
-      $('.addons-summary-container').html(addon.name + addon.quantity+'x' + '$'+addon.price.USD.unit_amount);
+      $('.addons-summary-container').append(addon.name + addon.quantity+'x' + '$'+addon.price.USD.unit_amount);
     }
     
   }
