2c75c03c14af82fe3498dd11ed1d6c91970afa7a
JAdriamooneyC Group <adriamooney@Macintosh.local>
2014-09-17 15:20:50 -0700

    advanced mobile in progress

--- app/assets/javascripts/minimal_form.js
+++ app/assets/javascripts/minimal_form.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   // On form submit, we stop submission to go get the token
   $('form').on('submit', function (event) {
-
-
     event.preventDefault();
 
     // Disable the submit button
@@ -12,46 +10,21 @@ $(document).ready(function() {
 
     var form = this;
 
-
-    if ($('#subscribe').hasClass('btn-submit')) {
-
+    if( $('#subscribe').hasClass('btn-submit') ) {
         // Now we call recurly.token with the form. It goes to Recurly servers
-        // to tokenize the credit card information, then injects the token into the
-        // data-recurly="token" field above
-        recurly.token(form, function (err, token) {
-          if (err) {
-            error(err);
-          } else {
-            create_subscription();
-          };
-        });
+      // to tokenize the credit card information, then injects the token into the
+      // data-recurly="token" field above
+      recurly.token(form, function (err, token) {
+        if (err) {
+          error(err);
+        } else {
+          create_subscription();
+        };
       });
-
-      // Identity card type
-      $("#number").on('change', function(event) {
-        var card_number = $("#number").val()
-          , card_type = recurly.validate.cardType(card_number)
-          , card_is_valid = recurly.validate.cardNumber($("#number").val())
-          , number_field = $('.customer-fields--card-number .form-input');
-
-        if(card_is_valid) {
-          $(number_field).removeClass('form-input__error');
-        }
-        else {
-          $(number_field).addClass('form-input__error');
-        }
-
-        if((card_type == 'default') || (card_type == 'unknown')) {
-          $('.icon-card').addClass('icon-card__generic');
-          $('.icon-card').removeClass('icon-card__visa icon-card__mastercard icon-card__amex icon-card__visa discover');
-        }
-        else {
-          $('.icon-card').removeClass('icon-card__generic');
-          $('.icon-card').addClass('icon-card__' + card_type);
-        }
-      });
-    } //end if
+    }//end if
+    
     else {
+
       recurly.paypal({ description: 'test' }, function (err, token) {
         if (err) {
           console.log(err);
@@ -65,7 +38,39 @@ $(document).ready(function() {
           form.submit();
         }
       });
-    } 
+
+    }
+
+    
+
+  });
+
+  
 
 
+
+
+  // Identity card type
+  $("#number").on('change', function(event) {
+    var card_number = $("#number").val()
+      , card_type = recurly.validate.cardType(card_number)
+      , card_is_valid = recurly.validate.cardNumber($("#number").val())
+      , number_field = $('.customer-fields--card-number .form-input');
+
+    if(card_is_valid) {
+      $(number_field).removeClass('form-input__error');
+    }
+    else {
+      $(number_field).addClass('form-input__error');
+    }
+
+    if((card_type == 'default') || (card_type == 'unknown')) {
+      $('.icon-card').addClass('icon-card__generic');
+      $('.icon-card').removeClass('icon-card__visa icon-card__mastercard icon-card__amex icon-card__visa discover');
+    }
+    else {
+      $('.icon-card').removeClass('icon-card__generic');
+      $('.icon-card').addClass('icon-card__' + card_type);
+    }
+  });
 });
