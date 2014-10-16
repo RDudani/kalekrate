43bdb3d29b69ab3b7f386642aa2408230fccd17b
Christopher Rogers <chrissrogers@gmail.com>
2014-10-16 14:21:02 -0700

    Fixes coupon entry
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- app/assets/javascripts/pricing.js
+++ app/assets/javascripts/pricing.js
@@ -1,51 +1,39 @@
 $(document).ready(function() {
   var pricing = recurly.Pricing();
+
   pricing.attach($('form'));
 
   // Plans
-  pricing.on('set.plan', planHandler);
-
-  function planHandler(plan) {
-    console.log(plan);
+  pricing.on('set.plan', function planHandler (plan) {
     $('.subscription-price').text('$'+plan.price.USD.unit_amount);
     showAddons(plan.addons);
     pricing.attach($('form'));
-  }
+  });
 
   // Addons
   $('body').on('focus', '.addons-list .addon-item--quantity', function() {
     $('.addons-items-summary').empty();
   });
 
-  pricing.on('set.addon', addonHandler);
-
-
-  function addonHandler(addon) {
-    console.log(addon);
+  pricing.on('set.addon', function addonHandler (addon) {
     showAddonsSummary(addon);
-    
-  }
-
-  //Coupon
-  $('#enter-coupon').click(function() {
-     pricing.on('set.coupon', couponHandler);
   });
- 
 
-  function couponHandler(coupon) {
-    console.log(coupon);
-    var num = (coupon.discount.amount.USD);
+  // Coupon
+  pricing.on('set.coupon', function couponHandler (coupon) {
+    var num = coupon.discount.amount.USD;
     var n = num.toFixed(2);
     $('.coupon li').html('<span class="coupon-code">'+coupon.code + '</span><span class="coupon-amount-off">-$'+n+'</span>');
-  }
+  });
 
   $('.coupon-text').click(function() {
     $('.coupon-none, .coupon-text').hide();
     $('.hidden').show();
+    $('.coupon-code').focus();
     return false;
   });
 
-  //Payment Option
+  // Payment Option
   $('.payment-type a').click(function() {
     var tab = $(this).attr('href');
     $('.payment-type a').removeClass('active');
@@ -53,10 +41,9 @@ $(document).ready(function() {
     $('.panel').hide();
     $(tab).show();
 
-    if($(this).attr('id') == 'payby-paypal') {
-        $('#subscribe').attr('class', 'paypal-submit');
-    }
-    else {
+    if ($(this).attr('id') == 'payby-paypal') {
+      $('#subscribe').attr('class', 'paypal-submit');
+    } else {
       $('#subscribe').attr('class', 'btn-submit');
     }
 
